2848d1528db681d26432f4c92cd4fdd0441c6c06
David Rauschenbach <davidr@imap.cc>
2017-09-14 19:07:01 -0700

    Implement client:receive('*a') to receive all content until the connection is closed

--- socketcore/clientreceive.go
+++ socketcore/clientreceive.go
@@ -2,6 +2,7 @@ package gluasocket_socketcore
 
 import (
 	"bytes"
+	"io"
 	"net"
 	"time"
 
@@ -10,43 +11,56 @@ import (
 
 func clientReceiveMethod(L *lua.LState) int {
 	client := checkClient(L)
-	luaPattern := L.Get(2)
-	//luaPrefix := "" // l.CheckString(3)
+	pattern := L.OptString(2, "")
+	//luaPrefix := "" // TODO l.CheckString(3)
 
-	if luaPattern.Type() == lua.LTString {
-		pattern, ok := luaPattern.(lua.LString)
-		if !ok {
-			L.Push(lua.LNil)
-			L.Push(lua.LString("Malformed pattern argument to socket:receive(pattern,...)"))
-			return 2
-		}
-		// Read a line of text from the socket. Line separators are not returned.
-		if pattern == "*l" {
+	// Read a line of text from the socket. Line separators are not returned.
+	if pattern == "*l" {
+		var buf bytes.Buffer
+		for {
 			if client.Timeout == 0 {
 				client.Conn.SetDeadline(time.Time{})
 			} else {
 				client.Conn.SetDeadline(time.Now().Add(client.Timeout))
 			}
-			var buf bytes.Buffer
-			for {
-				line, isPrefix, err := client.Reader.ReadLine()
-				if err != nil {
-					errstr := err.Error()
-					if err, ok := err.(net.Error); ok && err.Timeout() {
-						errstr = "timeout"
-					}
-					L.Push(lua.LNil)
-					L.Push(lua.LString(errstr))
-					return 2
+			line, isPrefix, err := client.Reader.ReadLine()
+			if err != nil {
+				errstr := err.Error()
+				if err, ok := err.(net.Error); ok && err.Timeout() {
+					errstr = "timeout"
 				}
-				buf.Write(line)
-				if !isPrefix {
-					break
-				}
+				L.Push(lua.LNil)
+				L.Push(lua.LString(errstr))
+				return 2
 			}
-			L.Push(lua.LString(string(buf.Bytes())))
-			return 1
+			buf.Write(line)
+			if !isPrefix {
+				break
+			}
 		}
+		L.Push(lua.LString(string(buf.Bytes())))
+		return 1
+	}
+
+	// Read until the connection is closed
+	if pattern == "*a" {
+		client.Conn.SetDeadline(time.Time{})
+		var buf bytes.Buffer
+		for {
+			byte, err := client.Reader.ReadByte()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				errstr := err.Error()
+				L.Push(lua.LNil)
+				L.Push(lua.LString(errstr))
+				return 2
+			}
+			buf.WriteByte(byte)
+		}
+		L.Push(lua.LString(string(buf.Bytes())))
+		return 1
 	}
 
 	L.RaiseError("client:receive() not implemented yet")
--- socketcore/clientreceive_test.go
+++ socketcore/clientreceive_test.go
@@ -17,7 +17,7 @@ func TestClientReceiveLines(t *testing.T) {
 	defer L.Close()
 	gluasocket.Preload(L)
 
-	listener, err := net.Listen("tcp", "localhost:0")
+	listener, err := net.Listen("tcp", "127.0.0.1:0")
 	assert.NoError(err)
 	port := listener.Addr().(*net.TCPAddr).Port
 
@@ -43,5 +43,39 @@ func TestClientReceiveLines(t *testing.T) {
 	assert.True(closed)
 	assert.Equal("abc", L.ToString(-2))
 	assert.Equal("123", L.ToString(-1))
+	assert.NoError(listener.Close())
+}
+
+func TestClientReceiveUntilClose(t *testing.T) {
+	assert := assert.New(t)
+	L := lua.NewState()
+	defer L.Close()
+	gluasocket.Preload(L)
+
+	listener, err := net.Listen("tcp", "localhost:0")
+	assert.NoError(err)
+	port := listener.Addr().(*net.TCPAddr).Port
+
+	accepted := false
+	closed := false
+	go func() {
+		if conn, err := listener.Accept(); err == nil {
+			accepted = true
+			conn.Write([]byte("abc\n"))
+			time.Sleep(5 * time.Millisecond)
+			conn.Write([]byte("123\n"))
+			time.Sleep(5 * time.Millisecond)
+			closed = true
+			conn.Close()
+		}
+	}()
+
+	script := fmt.Sprintf(`c=require 'socket.core'.connect('%s', %d); x=c:receive('*a'); c:close(); return x`, "127.0.0.1", port)
+	assert.NoError(L.DoString(script))
+
+	time.Sleep(20 * time.Millisecond)
+	assert.True(accepted)
+	assert.True(closed)
+	assert.Equal("abc\n123\n", L.ToString(-1))
 	assert.NoError(listener.Close())
 }
