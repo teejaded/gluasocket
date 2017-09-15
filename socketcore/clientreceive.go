64b401164d223b321d8112bf1df19dd83978849d
David Rauschenbach <davidr@imap.cc>
2017-09-14 19:24:51 -0700

    Implement client:receive(<number>), to read a fixed number of bytes from a socket

--- socketcore/clientreceive.go
+++ socketcore/clientreceive.go
@@ -11,11 +11,39 @@ import (
 
 func clientReceiveMethod(L *lua.LState) int {
 	client := checkClient(L)
-	pattern := L.OptString(2, "")
+	luaPattern := L.Get(2)
 	//luaPrefix := "" // TODO l.CheckString(3)
 
+	// Read a number of bytes from the socket
+	if luaPattern.Type() == lua.LTNumber {
+		if client.Timeout == 0 {
+			client.Conn.SetDeadline(time.Time{})
+		} else {
+			client.Conn.SetDeadline(time.Now().Add(client.Timeout))
+		}
+		var buf bytes.Buffer
+		for i := 0; i < L.ToInt(2); i++ {
+			byte, err := client.Reader.ReadByte()
+			if err == io.EOF {
+				break
+			}
+			if err != nil {
+				errstr := err.Error()
+				if err, ok := err.(net.Error); ok && err.Timeout() {
+					errstr = "timeout"
+				}
+				L.Push(lua.LNil)
+				L.Push(lua.LString(errstr))
+				return 2
+			}
+			buf.WriteByte(byte)
+		}
+		L.Push(lua.LString(string(buf.Bytes())))
+		return 1
+	}
+
 	// Read a line of text from the socket. Line separators are not returned.
-	if pattern == "*l" {
+	if luaPattern.Type() == lua.LTString && luaPattern.String() == "*l" {
 		var buf bytes.Buffer
 		for {
 			if client.Timeout == 0 {
@@ -43,8 +71,12 @@ func clientReceiveMethod(L *lua.LState) int {
 	}
 
 	// Read until the connection is closed
-	if pattern == "*a" {
-		client.Conn.SetDeadline(time.Time{})
+	if luaPattern.Type() == lua.LTString && luaPattern.String() == "*a" {
+		if client.Timeout == 0 {
+			client.Conn.SetDeadline(time.Time{})
+		} else {
+			client.Conn.SetDeadline(time.Now().Add(client.Timeout))
+		}
 		var buf bytes.Buffer
 		for {
 			byte, err := client.Reader.ReadByte()
@@ -53,6 +85,9 @@ func clientReceiveMethod(L *lua.LState) int {
 			}
 			if err != nil {
 				errstr := err.Error()
+				if err, ok := err.(net.Error); ok && err.Timeout() {
+					errstr = "timeout"
+				}
 				L.Push(lua.LNil)
 				L.Push(lua.LString(errstr))
 				return 2
