3cdbda910ca3399cbe8796e4dc7efcd8f604eacf
TJ Miller <millert@us.ibm.com>
2018-11-05 15:23:02 -0800

    fix socket.core skip off by one error

--- socketcore/skip_test.go
+++ socketcore/skip_test.go
@@ -15,7 +15,7 @@ func TestSkip0(t *testing.T) {
 	gluasocket.Preload(L)
 
 	assert.NoError(L.DoString(`return require 'socket.core'.skip(0, 1, 2, 3)`))
-	assert.Equal(4, L.GetTop())
+	assert.Equal(3, L.GetTop())
 	assert.Equal(1, L.ToInt(-3))
 	assert.Equal(2, L.ToInt(-2))
 	assert.Equal(3, L.ToInt(-1))
@@ -28,8 +28,19 @@ func TestSkip1(t *testing.T) {
 	gluasocket.Preload(L)
 
 	assert.NoError(L.DoString(`return require 'socket.core'.skip(1, 1, 2, 3)`))
-	assert.Equal(3, L.GetTop())
+	assert.Equal(2, L.GetTop())
 	assert.Equal(2, L.ToInt(-2))
+	assert.Equal(3, L.ToInt(-1))
+}
+
+func TestSkip2(t *testing.T) {
+	assert := assert.New(t)
+	L := lua.NewState()
+	defer L.Close()
+	gluasocket.Preload(L)
+
+	assert.NoError(L.DoString(`return require 'socket.core'.skip(2, 1, 2, 3)`))
+	assert.Equal(1, L.GetTop())
 	assert.Equal(3, L.ToInt(-1))
 }
 
@@ -40,5 +51,5 @@ func TestSkipPast(t *testing.T) {
 	gluasocket.Preload(L)
 
 	assert.NoError(L.DoString(`return require 'socket.core'.skip(9, 1, 2, 3)`))
-	assert.Equal(1, L.GetTop())
+	assert.Equal(0, L.GetTop())
 }
