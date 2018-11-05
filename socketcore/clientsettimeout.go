e3ee9c5fe16c60c851594f0c74ec55ff352797a2
TJ Miller <millert@us.ibm.com>
2018-11-05 15:22:39 -0800

    fix socket.core make settimeout compatible with try

--- socketcore/clientsettimeout.go
+++ socketcore/clientsettimeout.go
@@ -10,5 +10,6 @@ func clientSetTimeoutMethod(L *lua.LState) int {
 	client := checkClient(L)
 	timeout := L.CheckNumber(2)
 	client.Timeout = time.Duration(timeout * 1.0e9)
-	return 0
+	L.Push(lua.LNumber(1))
+	return 1
 }
