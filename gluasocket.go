e77cc754899925e4c76aec138544a688305620fc
David Rauschenbach <davidr@imap.cc>
2019-07-08 13:31:50 -0700

    OPS-196 GitHub org has been renamed

--- gluasocket.go
+++ gluasocket.go
@@ -1,18 +1,18 @@
 package gluasocket
 
 import (
-	"github.com/BixData/gluasocket/ltn12"
-	"github.com/BixData/gluasocket/mime"
-	"github.com/BixData/gluasocket/mimecore"
-	"github.com/BixData/gluasocket/socket"
-	"github.com/BixData/gluasocket/socketcore"
-	"github.com/BixData/gluasocket/socketexcept"
-	"github.com/BixData/gluasocket/socketftp"
-	"github.com/BixData/gluasocket/socketheaders"
-	"github.com/BixData/gluasocket/sockethttp"
-	"github.com/BixData/gluasocket/socketsmtp"
-	"github.com/BixData/gluasocket/sockettp"
-	"github.com/BixData/gluasocket/socketurl"
+	"github.com/nubix-io/gluasocket/ltn12"
+	"github.com/nubix-io/gluasocket/mime"
+	"github.com/nubix-io/gluasocket/mimecore"
+	"github.com/nubix-io/gluasocket/socket"
+	"github.com/nubix-io/gluasocket/socketcore"
+	"github.com/nubix-io/gluasocket/socketexcept"
+	"github.com/nubix-io/gluasocket/socketftp"
+	"github.com/nubix-io/gluasocket/socketheaders"
+	"github.com/nubix-io/gluasocket/sockethttp"
+	"github.com/nubix-io/gluasocket/socketsmtp"
+	"github.com/nubix-io/gluasocket/sockettp"
+	"github.com/nubix-io/gluasocket/socketurl"
 	"github.com/yuin/gopher-lua"
 )
 
