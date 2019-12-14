d05403e15f5905a6163ccde229b47bc8fccb9588
cyian-1756 <devnull64@vfemail.net>
2019-12-14 13:22:49 -0500

    Fixed possible crash caused by not checking array length

--- socketcore/dnstohostname.go
+++ socketcore/dnstohostname.go
@@ -16,7 +16,11 @@ func dnsToHostName(L *lua.LState) int {
 	}
 	// Generally only one result is returned by LookupAddr, so we just assume this is the case
 	// TODO: Return a table containing all hostnames mapping to the address
-	L.Push(lua.LString(host[0]))
+	if len(host) > 0 {
+		L.Push(lua.LString(host[0]))
+	} else {
+		L.Push(lua.LNil)
+	}
 	L.Push(lua.LNil)
 	return 2
 }
